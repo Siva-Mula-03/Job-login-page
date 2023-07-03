5e2b3bd7677f5658ccd2184e49ebcb11d35f92a7
Mula Siva Prasad Babu <111627965+Siva-Mula-03@users.noreply.github.com>
2023-07-03 09:29:37 +0530

    Add files via upload

---
+++ T2.js
@@ -0,0 +1,34 @@
+!function(t,e,n,c,a,i,r){
+    t[n]=t[n]||function(){
+      (t[n].q=t[n].q||[]).push(arguments)
+    },
+    (i=e.createElement(c)).async=1,
+    i.src="https://www.clarity.ms/tag/8btq1y5iw5",
+    (r=e.getElementsByTagName(c)[0]).parentNode.insertBefore(i,r)
+  }(window,document,"clarity","script");
+  
+  !function(){
+    function e(){
+      var e=.01*window.innerHeight;
+      document.documentElement.style.setProperty("--vh",e+"px")
+    }
+    e(),
+    window.addEventListener("resize",e),
+    window.addEventListener("orientationchange",e)
+  }();
+  
+  !function(e,t,a,n,g){
+    e[n]=e[n]||[],
+    e[n].push({
+      "gtm.start":(new Date).getTime(),
+      event:"gtm.js"
+    });
+    var m=t.getElementsByTagName(a)[0],
+    r=t.createElement(a);
+    r.async=!0,
+    r.src="https://www.googletagmanager.com/gtm.js?id=GTM-NDXQV7L",
+    m.parentNode.insertBefore(r,m)
+  }(window,document,"script","dataLayer");
+  
+  <script src="https://argyleink.github.io/scroll-timeline/dist/scroll-timeline.js" defer="defer"></script>
+  
